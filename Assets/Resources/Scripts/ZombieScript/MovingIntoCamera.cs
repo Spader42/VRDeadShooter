7982b142ed0730f093f0c2209e8b8198cdfd42c5
Toti <briend.maxime@hotmail.fr>
2018-02-05 22:59:39 +0100

    MBD ajout du zombie : - Model + animation - Scripts déplacement du zombie vers le joueur et spanwer de Zombie - Comportement du zombie (après s'être déplacé vers le joueur suffisament près, il se met à frapper)

---
+++ Assets/Resources/Scripts/ZombieScript/MovingIntoCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingIntoCamera : MonoBehaviour {
+
+    public Transform target;
+    public float nearEnoughFromTarget;
+    public float speed;
+    bool isnearToTarget;
+    Animator anim;
+
+    private void Start()
+    {
+        isnearToTarget = false;
+        anim = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (!isnearToTarget && !checkIfNearEnoughFromTarget())
+        {
+            float step = speed * Time.deltaTime;
+            Vector3 positionTargetWithoutYAxes = target.position;
+            positionTargetWithoutYAxes.y = transform.position.y;
+            transform.LookAt(positionTargetWithoutYAxes);
+            transform.position = Vector3.MoveTowards(transform.position, positionTargetWithoutYAxes, step);
+        }
+    }
+
+    bool checkIfNearEnoughFromTarget()
+    {
+        if (Vector3.Distance(target.position, this.transform.position) <= nearEnoughFromTarget)
+        {
+            anim.SetBool("nearToTarget", true);
+            isnearToTarget = true;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Resources/Scripts/ZombieScript/ZombieSpanwer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpanwer : MonoBehaviour {
+
+    public float spawnTime = 3f;
+    public int secBeforeFirstSpawn = 5;
+    public GameObject zombie;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(DelayBeforeSpawn());
+	}
+
+    IEnumerator DelayBeforeSpawn()
+    {
+        yield return new WaitForSeconds(secBeforeFirstSpawn);
+        InvokeRepeating("Spawn", 0, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void Spawn()
+    {
+        Instantiate(zombie, transform.position , transform.rotation);
+    }
+}
