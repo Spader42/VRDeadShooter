49083e21cc32397ce64c19106c5539a2b4fd4f24
Toti <briend.maxime@hotmail.fr>
2018-02-07 21:07:03 +0100

    MBD : Final cut Director's cut across the universe

--- Assets/Resources/Scripts/ZombieScript/EnemyHealth.cs
+++ Assets/Resources/Scripts/ZombieScript/EnemyHealth.cs
@@ -6,6 +6,9 @@ public class EnemyHealth : MonoBehaviour, IDamageable {
 
     public int startingHealth = 3;
     public GameObject scoreboard;
+    public GameObject magnum;
+    public GameObject ak47;
+    public GameObject pompe;
 
     private int currentHealth;
     Animator anim;
@@ -29,9 +32,29 @@ public class EnemyHealth : MonoBehaviour, IDamageable {
     public void Damage(int damage, Vector3 hitPoint)
     {
         currentHealth -= damage;
-        
+
         if (currentHealth <= 0)
         {
+            int bonus = UnityEngine.Random.Range(0, 3);
+            int random = UnityEngine.Random.Range(0, 2);
+            int bonusAmmo = 0;
+            if (bonus == 0)
+            {
+                bonusAmmo = UnityEngine.Random.Range(1, 2);
+            }
+            if (random == 0)
+            {
+                magnum.GetComponent<Gun>().ammo += 2 + bonusAmmo;
+                
+            }
+            else if (random == 1)
+            {
+                ak47.GetComponent<Gun>().ammo += 4 + bonusAmmo;
+            }
+            else if (random == 2)
+            {
+                pompe.GetComponent<Gun>().ammo += 1 + bonusAmmo;
+            }
             anim.SetBool("dead", true);
             BoxCollider boxCollider = gameObject.GetComponent<BoxCollider>();
             Vector3 size;
