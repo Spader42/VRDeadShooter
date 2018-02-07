9520366030fe7325842ca55ca5d91c122b75efb1
Toti22 <briend.maxime@hotmail.fr>
2018-02-07 16:51:52 +0100

    MBD : Fix oublie d'ajout du dossier player

---
+++ Assets/Resources/Scripts/Player/LifeManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeManager : MonoBehaviour {
+
+    public GameObject lifeText;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void takeDamage(int damage) {
+       int life = int.Parse(lifeText.GetComponent<TextMesh>().text);
+        life -= damage;
+        lifeText.GetComponent<TextMesh>().text = life.ToString();
+    }
+}
