096a2e5f897f9ce88066d2c5f5f34f91782fd6cd
Spader42 <steevenbrest@hotmail.com>
2018-02-08 09:33:04 +0100

    SDD : Ajout du son de game over et des coups des zombies

--- Assets/Resources/Scripts/Player/LifeManager.cs
+++ Assets/Resources/Scripts/Player/LifeManager.cs
@@ -14,11 +14,25 @@ public class LifeManager : MonoBehaviour {
     public GameObject ScoreFinalText;
     public GameObject LifeBar;
     public GameObject FinaleScore;
+    public AudioClip zombieHit;
+    public AudioSource sourceZombieHit;
+    public AudioClip gameOver;
+    public AudioSource sourceGameOver;
 
     // Use this for initialization
-    void Start () {
-        	
-	}
+    void Start ()
+    {
+        this.sourceZombieHit = this.gameObject.AddComponent<AudioSource>();
+        this.sourceGameOver = this.gameObject.AddComponent<AudioSource>();
+
+        // Stopping caudio from playing on awake
+        this.sourceZombieHit.playOnAwake = false;
+        this.sourceGameOver.playOnAwake = false;
+
+        // Assigning clips to their source
+        this.sourceZombieHit.clip = this.zombieHit;
+        this.sourceGameOver.clip = this.gameOver;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -31,6 +45,7 @@ public class LifeManager : MonoBehaviour {
         lifeText.GetComponent<TextMesh>().text = life.ToString();
         if (life <= 0)
         {
+            sourceGameOver.Play();
             cameraHolder.GetComponent<MenuCameraController>().SetMount(gameOverMount);
             weaponHolder.SetActive(false);
             ScoreHolder.SetActive(false);
@@ -39,10 +54,14 @@ public class LifeManager : MonoBehaviour {
             FinaleScore.SetActive(true);
             StartCoroutine(loadMenu());
         }
+        else
+        {
+            sourceZombieHit.Play();
+        }
     }
     IEnumerator loadMenu()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(7);
         SceneManager.LoadScene("SceneMenu");
     }
 }
